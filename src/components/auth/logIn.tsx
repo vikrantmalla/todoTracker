fb3c40e8717d2b4832c7903ba4ac10e962dcb0dc
bikrant malla <bikrant.malla@truenary.com>
2023-10-31 21:06:48 +0545

    feat: add a reset password popup

--- src/components/auth/logIn.tsx
+++ src/components/auth/logIn.tsx
@@ -2,11 +2,10 @@ import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../app/store";
-import { setShowModal, setShowForgetPasswordModal, setCredentials } from "../../features/authSlice";
+import { setShowModal, setCredentials } from "../../features/authSlice";
 import { useLoginMutation } from "../../features/userApiSlice";
 import { useNavigate } from "react-router-dom";
 import { LogInSubmitForm } from "../../types/form";
-import ResetPassword from "./resetPassword";
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -52,86 +51,72 @@ const LogIn = () => {
     }
   };
 
-  const showForgetPasswordModal = useSelector((state: RootState) => state.auth.showForgetPasswordModal);
+  // const showForgetPasswordModal = useSelector((state: RootState) => state.auth.showForgetPasswordModal);
 
-  const handleShowForgetPasswordModal = () => {
-    dispatch(setShowForgetPasswordModal(true));
-  };
-
-  return showForgetPasswordModal === false ? (
-    <>
-      <form onSubmit={handleSubmit(submit)}>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="email"
-          >
-            Email
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="Enter Email"
-            {...register("loginEmail", {
-              required: "Please enter your email",
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                message: "Please enter a valid email",
-              },
-            })}
-          />
-          {errors.loginEmail != null && (
-            <small className="error-message block text-red-600 mt-2">
-              {errors.loginEmail.message}
-            </small>
-          )}
-        </div>
-        <div className="mb-6">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="Enter Password"
-            {...register("loginPassword", {
-              required: "Please enter your password",
-            })}
-          />
-          {errors.loginPassword != null && (
-            <small className="error-message block text-red-600 mt-2 mx-0">
-              {errors.loginPassword.message}
-            </small>
-          )}
-        </div>
-        <div className="my-auto">
-          <p
-            className="mb-4 text-center font-bold text-sm text-blue-500 hover:text-blue-800"
-            onClick={handleShowForgetPasswordModal}
-          >
-            Forgot Password?
-          </p>
-          <button
-            className={`w-full font-bold py-2 px-4 h-[50px] ${isSubmitting
-              ? "bg-gray-100 text-black border border-black"
-              : "bg-blue-500 hover:bg-blue-700 text-white focus:outline-none focus:shadow-outline"
-              }`}
-            type="submit"
-            disabled={isSubmitting}
-          >
-            Log In
-          </button>
-        </div>
-      </form>
-    </>
-  ) : (
-    <ResetPassword />
-  );
+  return (
+    <form onSubmit={handleSubmit(submit)}>
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="email"
+        >
+          Email
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="email"
+          type="email"
+          placeholder="Enter Email"
+          {...register("loginEmail", {
+            required: "Please enter your email",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: "Please enter a valid email",
+            },
+          })}
+        />
+        {errors.loginEmail != null && (
+          <small className="error-message block text-red-600 mt-2">
+            {errors.loginEmail.message}
+          </small>
+        )}
+      </div>
+      <div className="mb-6">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          placeholder="Enter Password"
+          {...register("loginPassword", {
+            required: "Please enter your password",
+          })}
+        />
+        {errors.loginPassword != null && (
+          <small className="error-message block text-red-600 mt-2 mx-0">
+            {errors.loginPassword.message}
+          </small>
+        )}
+      </div>
+      <div className="my-auto">
+        <button
+          className={`w-full font-bold py-2 px-4 h-[50px] ${isSubmitting
+            ? "bg-gray-100 text-black border border-black"
+            : "bg-blue-500 hover:bg-blue-700 text-white focus:outline-none focus:shadow-outline"
+            }`}
+          type="submit"
+          disabled={isSubmitting}
+        >
+          Log In
+        </button>
+      </div>
+    </form>
+  )
 };
 
 export default LogIn;
--- src/components/auth/resetPassword.tsx
+++ src/components/auth/resetPassword.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { AppDispatch } from "../../app/store";
-import { setCredentials, setShowForgetPasswordModal, setShowModal } from "../../features/authSlice";
+import { setCredentials, setShowModal } from "../../features/authSlice";
 import { LogInSubmitForm } from "../../types/form";
 import { useUpdateUserMutation } from "../../features/userApiSlice";
 import { toast } from 'react-toastify'
@@ -46,69 +46,87 @@ const ResetPassword = () => {
   const dispatch = useDispatch<AppDispatch>();
 
   return (
-    <form onSubmit={handleSubmit(submit)}>
-      <div className="mb-4">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="email"
+    <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="flex items-center justify-center min-h-screen px-4 pt-6 pb-20 text-center sm:block sm:p-0">
+        <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+          <div className="absolute inset-0  bg-gray-500 opacity-75"></div>
+        </div>
+        <span
+          className="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
         >
-          Email
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="email"
-          type="email"
-          placeholder="Enter Email"
-          {...register("loginEmail", {
-            required: "Please enter your email",
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-              message: "Please enter a valid email",
-            },
-          })}
-        />
-        {errors.loginEmail != null && (
-          <small className="error-message block text-red-600 mt-2">
-            {errors.loginEmail.message}
-          </small>
-        )}
-      </div>
-      <div className="mb-6">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="password"
+          &#8203;
+        </span>
+        <div
+          className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
         >
-          Password
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          id="password"
-          type="password"
-          placeholder="Enter Password"
-          {...register("loginPassword", {
-            required: "Please enter your password",
-          })}
-        />
-        {errors.loginPassword != null && (
-          <small className="error-message block text-red-600 mt-2 mx-0">
-            {errors.loginPassword.message}
-          </small>
-        )}
+          <div>
+            <form onSubmit={handleSubmit(submit)}>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Enter Email"
+                  {...register("loginEmail", {
+                    required: "Please enter your email",
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                      message: "Please enter a valid email",
+                    },
+                  })}
+                />
+                {errors.loginEmail != null && (
+                  <small className="error-message block text-red-600 mt-2">
+                    {errors.loginEmail.message}
+                  </small>
+                )}
+              </div>
+              <div className="mb-6">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Enter Password"
+                  {...register("loginPassword", {
+                    required: "Please enter your password",
+                  })}
+                />
+                {errors.loginPassword != null && (
+                  <small className="error-message block text-red-600 mt-2 mx-0">
+                    {errors.loginPassword.message}
+                  </small>
+                )}
+              </div>
+              <div className="my-auto">
+                <button
+                  className={`w-full font-bold py-2 px-4 h-[50px] ${isSubmitting
+                    ? "bg-gray-100 text-black border border-black"
+                    : "bg-blue-500 hover:bg-blue-700 text-white focus:outline-none focus:shadow-outline"
+                    }`}
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Reset Password
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-      <div className="my-auto">
-        <button
-          className={`w-full font-bold py-2 px-4 h-[50px] ${isSubmitting
-            ? "bg-gray-100 text-black border border-black"
-            : "bg-blue-500 hover:bg-blue-700 text-white focus:outline-none focus:shadow-outline"
-            }`}
-          type="submit"
-          disabled={isSubmitting}
-        >
-          Reset Password
-        </button>
-      </div>
-      <p className="text-center mt-2 ont-bold text-sm text-blue-500 hover:text-blue-800" onClick={() => dispatch(setShowForgetPasswordModal(false))}>Back to Log In</p>
-    </form>
+    </div>
   );
 };
 
